3048f39447663c18fd849052263817827183ee0d
Gabor Babicz <gabor.babicz@gmail.com>
2014-12-15 12:32:57 +0100

    Don’t rely on prototype extension for didInsertElement

--- addon/components/lazy-video.js
+++ addon/components/lazy-video.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+var on = Ember.on;
 
 var get = Ember.get;
 var set = Ember.set;
@@ -23,14 +24,14 @@ export default Ember.Component.extend({
     return providers.getUrl(provider, 'embedUrl', videoId, { autoplay: 1 });
   }),
 
-  _getVideoThumbnail: (function() {
+  _getVideoThumbnail: on('didInsertElement', function() {
     var providers = get(this, 'providers');
     var provider  = get(this, 'provider');
     var videoId   = get(this, 'videoId');
     providers.get(provider).thumbnailUrl(videoId).then(function(res) {
       this.set('videoThumbnail', res);
     }.bind(this));
-  }).on('didInsertElement'),
+  }),
 
   style: Ember.computed('videoThumbnail', function() {
     var thumbnail = get('videoThumbnail');
