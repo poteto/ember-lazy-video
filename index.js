1fc4079ac19eb645435c7e09574c9e1a37208363
Brian Runnells <brunnells@heroku.com>
2016-11-11 14:37:11 -0600

    move css import back inside addon

--- index.js
+++ index.js
@@ -2,9 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-lazy-video',
-  included(app) {
-    this._super.included(app);
-    app.import('vendor/lazy-video/lazy-video.css');
-  }
+  name: 'ember-lazy-video'
 };
